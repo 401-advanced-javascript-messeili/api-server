cfce916e00f8b9031be842d3e1d3814ec86a6c69
messeili <mymoa.2014@gmail.com>
2020-10-19 19:19:54 +0300

    lab07

---
+++ middleware/500.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.statusMessage = 'Error :(';
+  res.json({ error: `${err.message} ${req.requestTime}` });
+}
+
+module.exports = errorHandler;
---
+++ middleware/timestamp.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function requestTime(req, res, next) {
+  req.requestTime = new Date().toDateString();
+  console.log(req.requestTime);
+  next();
+}
+
+module.exports = requestTime;
